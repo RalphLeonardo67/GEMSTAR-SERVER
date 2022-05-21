7856142b966a4e24217b0982b56bb3738441b4fd
RalphLeonardo67 <rvleonardo607@gmail.com>
2022-05-21 19:28:46 +0800

    Update app.js

--- controller/app.js
+++ controller/app.js
@@ -2,19 +2,19 @@ const { executeScript } = require("../model/mysql/utils");
 const appQuery = require("../model/mysql/query/app");
 
 const createLogs = (logsId, logsType, remarks) => {
-  return executeScript(appQuery.insertLogs(logsId, logsType, remarks));
+    return executeScript(appQuery.insertLogs(logsId, logsType, remarks));
 };
 
 const fetchLogs = () => {
-  return Promise.all([
-    executeScript(appQuery.fetchLogs(0, 25)),
-    executeScript(appQuery.logsTotalCount()),
-  ]).then(([data, total_count]) => {
-    return { data, totalCount: total_count[0].total_count };
-  });
+    return Promise.all([
+        executeScript(appQuery.fetchLogs(0, 25)),
+        executeScript(appQuery.logsTotalCount()),
+    ]).then(([data, total_count]) => {
+        return { data, totalCount: total_count[0].total_count };
+    });
 };
 
 module.exports = {
-  createLogs,
-  fetchLogs,
+    createLogs,
+
 };
